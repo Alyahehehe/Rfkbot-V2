9b7c5c0da289ce6d9d0f0b6013f62d19925e45cc
Alyahehehe <108801766+Alyahehehe@users.noreply.github.com>
2022-07-06 20:35:08 +0700

    belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -6,17 +6,17 @@ let handler = async (m, { conn }) => {
 ╠➥ *2 Bulan* :      *Rp 30000*
 ╠➥ *Permanen* : *Rp 40000*
 ╠➥ *Owner* :   *Rp 30000*/bln
-╠➥ *Nomer Gopay & pulsa* : 
-║    085730903853
+╠➥ *Nomer Dana & pulsa* : 
+║    0895324080018
 ╠═〘 PEMBAYARAN 〙 ═
 ╠➥ Dana Dan Pulsa
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
-╠═ ©2021 Rpg wabot-aq
+╠═ ©2022 Rpg wabot-aq
 ╠═ Scrip original by Nurutomo
-╠═〘 AlyaaXzy 〙 ═`.trim(), watermark, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
+╠═〘 aresmodz 〙 ═`.trim(), watermark, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
 }
 
 handler.command = /^sewabot$/i
